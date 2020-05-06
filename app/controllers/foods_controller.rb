4b0fe08d6cc1710539912ec0ba956c04f4d7ff80
ryuichi-yamamoto <yamamotoryuichi0802@gmail.com>
2020-05-06 09:56:29 +0900

    2020.05.06

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -28,9 +28,6 @@ class FoodsController < ApplicationController
     @food = Food.find(params[:id])
   end
 
-  def editprofile
-  end
-
   def update
     food = Food.find(params[:id])
     food.update(food_params)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   collection do
       get 'newprofile'
       get 'indexprofile'
-      get 'editprofile'
     end
   end
 end
